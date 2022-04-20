244ca0d4e12a0d0e2af878a639ab1f22ee229351
Guilherm Consentino <guiconsentino@hotmail.com>
2022-04-20 18:21:40 -0300

    aprendendo e aplicando

---
+++ For1.java
@@ -0,0 +1,19 @@
+package controle;
+
+public class For1 {
+
+	public static void main(String[] args) {
+
+		for (int contador = 1; contador <= 10; contador += 2) {
+			System.out.printf("i = [%d]\n", contador);
+		}
+
+		/*                                   !a,b,c!
+		 * char letra = 'a'; 
+		 * for(;letra <= 'z';) {
+		 * System.out.println("letra = " + letra); 
+		 * letra++; }
+		 */
+	}
+}
+
